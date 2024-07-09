b81d4b30b4b764712b3e103495eeae5daabcb72d
zorodendron <151568679+zorodendron@users.noreply.github.com>
2024-07-09 19:08:49 +0200

    Add files via upload

--- lib/ModalProp.hs
+++ lib/ModalProp.hs
@@ -2,7 +2,7 @@ module ModalProp where
 
 data Prop =
         T | F
-          | P String -- Another proposition.
+          | P String 
           | Var String
           | Not Prop
           | And Prop Prop
@@ -21,6 +21,7 @@ type Predicate = String
 data Function = Function { arguments :: [Prop], returnValue :: Prop} deriving (Show, Eq)
 
 -- MODAL LOGIC
+
 data ModalOperator = Necessarily | Possibly
 
 type World = [Prop]
